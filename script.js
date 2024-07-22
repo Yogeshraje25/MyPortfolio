8e023d81152948fad90f871e1d7e12ed641d3bd1
Yogeshraje25 <raje.yogi25@gmail.com>
2024-07-22 12:06:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  
+ const typed = new Typed(".text",{
+    strings:["FRONT-END DEVELOPER","WEB DEVELOPER","FULL-STACK DEVELOPER"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+     
+     
+
+ })
